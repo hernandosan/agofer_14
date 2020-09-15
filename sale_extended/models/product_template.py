0fd41ec6c24efb2b4ee385ec82ee5730695eedaa
Juan Pablo Arcos Merchan <jparcos@lenovob2a0.bogota.agofer>
2020-09-15 17:08:34 -0500

    PB0019 [ADD] sale_extended: add dates (upload, delivery) in sale order

---
+++ sale_extended/models/product_template.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    sale_delay = fields.Float(default=3)
+    upload_delay = fields.Float('Customer Upload Time', default=1)
+    delivery_delay = fields.Float('Customer Delivery Time', default=2)
