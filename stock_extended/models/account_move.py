05a7fbb40b4a203c214b600b1c305a29ade5e1b5
Juan Pablo Arcos Merchan <jparcos@lenovob2a0.bogota.agofer>
2020-10-03 12:17:54 -0500

    PB0025 [REF] stock_extended: modify delivery guides

--- stock_extended/models/account_move.py
+++ stock_extended/models/account_move.py
@@ -7,5 +7,20 @@ class AccountMove(models.Model):
     _inherit = 'account.move'
 
     picking_id = fields.Many2one('stock.picking', readonly=True, copy=False)
-    guides_ids = fields.Many2many('delivery.guide', 'guide_invoice_rel', 'invoice_id', 'guide_id', 'Guides', copy=False)
-    delivery_state = fields.Selection([('pending','Pending'),('progress','Progress'),('delivered','Delivered')], 'Delivery state', copy=False)
+    delivery_state = fields.Selection([
+        ('pending','Pending'),
+        ('progress','Progress'),
+        ('cancel','Cancel'),
+        ('delivered','Delivered')], 'Delivery state', copy=False)
+
+    def action_delivery_confirm(self):
+        for move in self:
+            move._action_delivery('delivered')
+
+    def action_delivery_cancel(self):
+        for move in self:
+            move._action_delivery('cancel')
+
+    def _action_delivery(self, state):
+        self.ensure_one()
+        self.write({'delivery_state': state})
