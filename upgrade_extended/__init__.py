db8dec29f4b8d4db3599733bb7d2856d94ac9b94
Reyes Hernando Santana Perez <rhsantana@lenovoc1ab.bogota.agofer>
2020-12-03 11:03:26 -0500

    14.0 [REF] Update querys

--- upgrade_extended/__init__.py
+++ upgrade_extended/__init__.py
@@ -2,6 +2,7 @@
 
 import logging
 import os
+from datetime import date, datetime
 
 from . import controllers
 from . import models
@@ -11,20 +12,23 @@ directory = os.path.dirname(__file__)
 
 
 def pre_init_hook(cr):
+    _logger.warning('Query before')
     path = os.path.join(directory, 'querys/before')
     for file in sorted(os.listdir(path)):
         with open(os.path.join(path, file), 'r') as query_file:
-            query = query_file.read()
-            message = 'Insert into table: '
-            _logger.info(message + file)
-            cr.execute(query)
+            cr.execute(query_file.read())
 
 
 def post_init_hook(cr, result):
+    # PSQL
     path = os.path.join(directory, 'querys/after')
-    for file in sorted(os.listdir(path)):
+    total = len(os.listdir(path))
+    start = datetime.now()
+    _logger.warning('Query after, Total: %s, Start: %s' % (total, start))
+    for i, file in enumerate(sorted(os.listdir(path))):
+        _logger.warning('Table: %s, Progress: %s', (file, round(i*100/total, 2)))
         with open(os.path.join(path, file), 'r') as query_file:
-            query = query_file.read()
-            message = 'Insert into table: '
-            _logger.warning(message + file)
-            cr.execute(query)
+            cr.execute(query = query_file.read())
+    end = datetime.now()
+    time = start - end
+    _logger.warning('Query after, End: %s, Time: %s' % (end, round(time.seconds/60, 2)))
