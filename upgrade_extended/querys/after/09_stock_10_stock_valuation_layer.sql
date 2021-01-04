83e9e748556c32111b936536d34d19e7322f421f
Odoo <odoo@agoer.com.co>
2021-01-04 22:50:18 +0000

    14.0 [ADD] upgrade_extended: add stock.valuation.layer query

--- upgrade_extended/querys/after/09_stock_10_stock_valuation_layer.sql
+++ upgrade_extended/querys/after/09_stock_10_stock_valuation_layer.sql
@@ -1,52 +1,45 @@
-INSERT INTO stock_valuation_layer (
-    company_id,
-    product_id,
-    create_date,
-    quantity,
-    unit_cost,
-    value,
-    stock_move_id,
-    description
-) SELECT
-    sm.company_id,
-    sm.product_id,
-    sm.date,
-    sm.product_uom_qty,
-    sm.price_unit,
-    sm.price_unit * sm.product_uom_qty,
-    sm.id,
-    agofer.name
-    from stock_move sm
-INNER JOIN dblink('dbname=agofer_08','select
-    sm.id,
-    sm.company_id,
-    sm.state,
-    pt.type,
-    sls.usage as sls_usage,
-    sls.company_id as sls_company_id,
-    sld.usage as sld_usage,
-    sld.company_id as sld_company_id,
-    sp.name
-    FROM stock_move sm
-INNER JOIN product_product pp ON pp.id = sm.product_id
-INNER JOIN product_template pt ON pt.id = pp.product_tmpl_id
-INNER JOIN stock_location sls ON sls.id = sm.location_id
-INNER JOIN stock_location sld ON sld.id = sm.location_dest_id
-LEFT JOIN stock_picking sp ON sp.id = sm.picking_id;'
-) AS agofer (
-    id integer,
-    company_id integer,
-    state character varying,
-    type character varying,
-    sls_usage character varying,
-    sls_company_id integer,
-    sld_usage character varying,
-    sld_company_id integer,
-    name character varying
-) ON sm.id = agofer.id
-WHERE agofer.state = 'done'
-AND agofer.type = 'product'
-AND NOT (agofer.sls_usage = 'internal' OR (agofer.sls_usage = 'transit' AND agofer.sls_company_id IS NOT NULL))
-AND (agofer.sld_usage = 'internal' OR (agofer.sld_usage = 'transit' AND agofer.sld_company_id IS NOT NULL));
-
-select setval('stock_quant_id_seq', (select max(id) from stock_quant));
+insert into stock_valuation_layer (create_date, 
+write_uid, 
+write_date, 
+create_uid, 
+company_id, 
+product_id,
+value,
+quantity,
+description)
+select agofer.create_date, 
+agofer.write_uid, 
+agofer.write_date, 
+agofer.create_uid, 
+agofer.company_id, 
+agofer.product_id,
+agofer.cost,
+agofer.qty,
+cast(agofer.id as character varying)
+from dblink('dbname=agofer_08',
+            'select sq.id, 
+sq.create_date, 
+sq.write_uid, 
+sq.write_date, 
+sq.create_uid, 
+sq.company_id, 
+sq.product_id,
+pt.uom_id,
+sq.cost,
+sq.qty,
+sl.usage
+from stock_quant sq 
+inner join stock_location sl on sl.id = sq.location_id 
+inner join product_product pp on pp.id = sq.product_id 
+inner join product_template pt on pt.id = pp.product_tmpl_id;') as agofer(id integer, 
+create_date timestamp without time zone, 
+write_uid integer, 
+write_date timestamp without time zone, 
+create_uid integer, 
+company_id integer, 
+product_id integer, 
+uom_id integer,
+cost double precision,
+qty double precision,
+usage character varying) 
+where agofer.usage = 'internal';
