9fcde0ea4e8f31e81e2358c198ee2d07481d3f2e
Odoo <odoo@agoer.com.co>
2021-01-05 14:18:18 +0000

    14.0 [REF] delete accounht menus, add module account_menu

--- account_extended/wizard/account_consignment_wizard.py
+++ account_extended/wizard/account_consignment_wizard.py
@@ -7,8 +7,8 @@ class AccountConsignmentWizard(models.TransientModel):
     _name = 'account.consignment.wizard'
     _description = 'Account Consignment Wizard'
 
-    consignments_ids = fields.Many2many('account.consignment' 'Consignments', 
+    consignments_ids = fields.Many2many('account.consignment', string='Consignments', 
         default=lambda self: self.env['account.consignment'].browse(self._context.get('active_id')))
-    
+
     def action_confirm(self):
         self.consignments_ids.action_done()
