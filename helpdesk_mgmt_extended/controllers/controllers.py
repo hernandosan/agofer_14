4329decdd450292885e93097f2396f413797b239
Reyes Hernando Santana Perez <rhsantana@lenovoc1ab.bogota.agofer>
2021-02-03 11:11:56 -0500

    14.0 [REF]helpdesk_mgmt_extended: Cambioes en la vista /new/ticket

--- helpdesk_mgmt_extended/controllers/controllers.py
+++ helpdesk_mgmt_extended/controllers/controllers.py
@@ -5,9 +5,9 @@ from odoo.addons.helpdesk_mgmt.controllers.main import HelpdeskTicketController
 class CustomHelpdeskTicketController(HelpdeskTicketController):
     @http.route("/new/ticket", type="http", auth="user", website=True)
     def create_new_ticket(self, **kw):
-        categories = http.request.env["helpdesk.ticket.category"].search(
-            [("active", "=", True), ('team_id', '=', )])
         types = http.request.env["helpdesk.ticket.type"].search([])
+        categories = http.request.env["helpdesk.ticket.category"].search(
+            [("active", "=", True)])
         email = http.request.env.user.email
         name = http.request.env.user.name
         return http.request.render(
