a6c80849a70bbe9583d72470b1df885e0b34c5e7
Reyes Hernando Santana Perez <rhsantana@lenovoc1ab.bogota.agofer>
2021-02-03 16:10:36 -0500

    14.0[REF]stock_extended: Actualizacion Metodos de Envio

--- stock_extended/models/delivery_carrier.py
+++ stock_extended/models/delivery_carrier.py
@@ -9,9 +9,9 @@ class DeliveryCarrier(models.Model):
     name = fields.Char(required=False)
     carrier_type = fields.Selection([('stock','Stock'), ('purchase','Purchase')], 'Delivery Type', default='stock')
     partner_id = fields.Many2one('res.partner', 'Carrier')
-    warehouse_id = fields.Many2one('stock.warehouse', 'Source Location')
-    warehouse_dest_id = fields.Many2one('stock.warehouse', 'Destination Location')
-    destination = fields.Char('Destination')
+    warehouse_id = fields.Many2one('res.city', 'Source Location')
+    warehouse_dest_id = fields.Many2one('res.city', 'Destination Location')
+    # destination = fields.Char('Destination')
     price_kg = fields.Float('Price (Kg)')
     tolerance = fields.Float('Tolerance (%)', help='Tolerance allowed in price.')
     notes = fields.Text('Terms and Conditions')
@@ -19,20 +19,20 @@ class DeliveryCarrier(models.Model):
     @api.model
     def create(self, vals):
         if vals.get('carrier_type') and vals.get('carrier_type') == 'stock':
-            warehouse_id = self.env['stock.warehouse'].browse(vals.get('warehouse_id')).name or ''
-            destination = vals.get('destination') or ''
+            warehouse_id = self.env['res.city'].browse(vals.get('warehouse_id')).name or ''
+            warehouse_dest_id = self.env['res.city'].browse(vals.get('warehouse_dest_id')).name or ''
             price = str(vals.get('price_kg') or '')
-            name = '%s -> %s $ %s' % (warehouse_id, destination, price)
+            name = '%s -> %s $ %s' % (warehouse_id, warehouse_dest_id, price)
             vals.update(name=name)
         return super(DeliveryCarrier, self).create(vals)
 
     def write(self, vals):
-        if vals.get('carrier_type') or vals.get('warehouse_id') or vals.get('destination') or vals.get('price_kg'):
+        if vals.get('carrier_type') or vals.get('city_id') or vals.get('price_kg'):
             for carrier in self:
                 if vals.get('carrier_type') == 'stock' or carrier.carrier_type == 'stock':
-                    warehouse_id = self.env['stock.warehouse'].browse(vals.get('warehouse_id')).name or carrier.warehouse_id.id
-                    destination = vals.get('destination') or carrier.destination
+                    warehouse_id = self.env['res.city'].browse(vals.get('warehouse_id')) or carrier.city_id.id
+                    warehouse_dest_id = self.env['res.city'].browse(vals.get('warehouse_dest_id')).name or carrier.city_id.id
                     price = str(vals.get('price_kg') or carrier.fixed_price)
-                    name = '%s -> %s $ %s' % (warehouse_id, destination, price)
+                    name = '%s -> %s $ %s' % (warehouse_id, warehouse_dest_id, price)
                     carrier.write({'name': name})
         return super(DeliveryCarrier, self).write(vals)
