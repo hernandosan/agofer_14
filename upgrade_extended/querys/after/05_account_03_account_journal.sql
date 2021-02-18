70559c2b970a9a8413115aa8599c8fd34ede72ee
Jparcos <jparcos@agofer.com.co>
2021-02-18 17:38:17 +0000

    210218 [REF] upgrade_extended: modify querys, not delete table related with res_company

--- upgrade_extended/querys/after/05_account_03_account_journal.sql
+++ upgrade_extended/querys/after/05_account_03_account_journal.sql
@@ -1,6 +1,51 @@
-ALTER TABLE account_journal DISABLE TRIGGER ALL;
-DELETE FROM account_journal;
-ALTER TABLE account_journal ENABLE TRIGGER ALL;
+-- ALTER TABLE account_journal DISABLE TRIGGER ALL;
+-- DELETE FROM account_journal;
+-- ALTER TABLE account_journal ENABLE TRIGGER ALL;
+
+UPDATE account_journal AS aj 
+SET code = agofer.code, 
+	create_date = agofer.create_date, 
+	write_uid = agofer.write_uid, 
+	loss_account_id = agofer.loss_account_id, 
+	write_date = agofer.write_date, 
+	create_uid = agofer.create_uid, 
+	name = agofer.name, 
+	company_id = agofer.company_id, 
+	profit_account_id = agofer.profit_account_id, 
+	type = agofer.type,
+	sequence_id = agofer.sequence_id,
+	currency_id = agofer.currency 
+FROM dblink('dbname=agofer_08','select
+	id, 
+	code, 
+	create_date, 
+	write_uid, 
+	loss_account_id, 
+	write_date, 
+	create_uid, 
+	name, 
+	company_id, 
+	profit_account_id, 
+	type,
+	sequence_id,
+	currency
+	from account_journal;'
+) AS agofer(
+	id integer, 
+	code character varying, 
+	create_date timestamp without time zone, 
+	write_uid integer, 
+	loss_account_id integer, 
+	write_date timestamp without time zone, 
+	create_uid integer, 
+	name character varying, 
+	company_id integer, 
+	profit_account_id integer, 
+	type character varying,
+	sequence_id integer,
+	currency integer
+)
+WHERE aj.id = agofer.id;
 
 INSERT INTO account_journal (
 	id, 
@@ -68,6 +113,6 @@ FROM dblink('dbname=agofer_08','select
 	type character varying,
 	sequence_id integer,
 	currency integer
-);
+) WHERE agofer.id not in (SELECT id from account_journal);
 
 select setval('account_journal_id_seq', (select max(id) from account_journal));
--- upgrade_extended/querys/after/05_account_19_account_inconterms.sql
+++ upgrade_extended/querys/after/05_account_19_account_inconterms.sql
@@ -1,6 +1,36 @@
-ALTER TABLE account_incoterms DISABLE TRIGGER ALL;
-DELETE FROM account_incoterms;
-ALTER TABLE account_incoterms ENABLE TRIGGER ALL;
+-- ALTER TABLE account_incoterms DISABLE TRIGGER ALL;
+-- DELETE FROM account_incoterms;
+-- ALTER TABLE account_incoterms ENABLE TRIGGER ALL;
+
+UPDATE account_incoterms AS ai 
+SET create_uid = agofer.create_uid,
+    code = agofer.code,
+    create_date = agofer.create_date,
+    name = agofer.name,
+    write_uid = agofer.write_uid,
+    write_date = agofer.write_date,
+    active = agofer.active
+FROM dblink('dbname=agofer_08', 'select
+	id,
+    create_uid,
+    code,
+    create_date,
+    name,
+    write_uid,
+    write_date,
+    active
+    from stock_incoterms;'
+) AS agofer (
+	id integer,
+    create_uid integer,
+    code character varying,
+    create_date timestamp without time zone,
+    name character varying,
+    write_uid integer,
+    write_date timestamp without time zone,
+    active boolean
+)
+WHERE ai.id = agofer.id;
 
 INSERT INTO account_incoterms (
 	id,
@@ -39,6 +69,6 @@ FROM dblink('dbname=agofer_08', 'select
     write_uid integer,
     write_date timestamp without time zone,
     active boolean
-);
+) where agofer.id not in (select id from account_incoterms);
 
 select setval('account_incoterms_id_seq', (select max(id) from account_incoterms));
